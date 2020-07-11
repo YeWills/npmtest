a48f3b0afd31f2cf4ce218807bee54e957ff717b
hz-qq2 <601661706@qq.com>
2020-07-11 22:06:23 +0800

    npm to git

---
+++ packages/yewBtn/.fatherrc.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2019-12-10 14:27:16
+ * @Author: Scm <chengqiuhua@erayt.com>
+ * @Copyright: 2015-2019 Erayt, Inc.
+ * @Description: If you have some questions, please contact: chengqiuhua@erayt.com.
+ */
+export default {
+  cjs: { type: 'babel', lazy: true },
+};
---
+++ packages/yewBtn/README.md
@@ -0,0 +1,6 @@
+# 介绍
+
+封装常见组件比如: EButton,ESelect,EModal,ETable,crudSearch,ESpan,ESchemaForm,ESchemaTable,EqueryForm等。
+
+# changeLog
+
