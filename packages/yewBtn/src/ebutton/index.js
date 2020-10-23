86c23580656058ba7b4e5d6ccac588346d882aab
hz-qq2 <601661706@qq.com>
2020-10-23 16:23:07 +0800

    测试10.24

--- packages/yewBtn/src/ebutton/index.js
+++ packages/yewBtn/src/ebutton/index.js
@@ -1,14 +1,8 @@
-/*
- * @Date: 2020-07-11 21:19:56
- * @Author: Scm <chengqiuhua@erayt.com>
- * @Copyright: 2015-2020 Erayt, Inc.
- * @Description: If you have some questions, please contact: chengqiuhua@erayt.com.
- */
 import React from 'react';
 import { Button } from 'antd';
 
 export default props => {
-  const { text = '提交1' } = props;
+  const { text = '提交10 month 24' } = props;
   return (
     <div>
       <span>{text}</span>
